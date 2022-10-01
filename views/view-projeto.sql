73fe67b5bd6d95923c7888a424352b2da6c4112b
SteffanoP <steffanoxpereira@gmail.com>
2022-10-01 10:58:34 -0300

    refactor: mata a view de @multi-megaman

--- views/view-projeto.sql
+++
@@ -1,9 +0,0 @@
-use lanterna_verde;
-
-
-#View para pegar o relatório de uma empresas
-CREATE OR REPLACE VIEW relatorio_empresa AS
-SELECT empresa.cnpj, fazanalise.comentario, fazanalise.score, fazanalise.deadline
-FROM empresa INNER JOIN fazanalise on empresa.cnpj = fazanalise.cnpjEmpresa;
-
-select * from relatorio_empresa where cnpj = "146745612";
